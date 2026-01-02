1658cb0863ef9c6b5eecd15f7328ca8a31cb0733
Alexandre Oliveira <alexandre.oliveira@gmail.com>
2026-01-02 17:06:50 -0300

    Bump version to 2026.1.4 and update release references
    Updated all Maven and build files to use version 2026.1.4 instead of 2025.4.3. Adjusted GitHub Actions, Dependabot, and release scripts to target the new 2026.1 branch and version. Also updated copyright years and download URLs to match the new release. Removed unused javax.persistence-api dependency from aem-plugin.

--- upload_to_relese.cmd
+++ upload_to_relese.cmd
@@ -1,7 +1,7 @@
-set TAG_NAME=v2025.4
+set TAG_NAME=v2026.1
 mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}.${parsedVersion.nextBuildNumber} versions:commit
 mvn clean package
-rem gh release create v2025.4 --generate-notes
+rem gh release create v2026.1 --generate-notes
 gh release upload %TAG_NAME% aem\aem-plugin\target\aem-plugin.jar --clobber
 gh release upload %TAG_NAME% connector\connector-app\target\dumont-connector.jar --clobber
 gh release upload %TAG_NAME% db\db-app\target\dumont-db.jar --clobber
